6d9c36cbb5559682c97f2db76febad30cff63d1a
Dhiva <dhivalogu@gmail.com>
2021-03-30 00:59:47 +0530

    initial commit

--- src/app/customer/customer-upload/customer-upload.component.ts
+++ src/app/customer/customer-upload/customer-upload.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-customer-upload',
   templateUrl: './customer-upload.component.html',
   styleUrls: ['./customer-upload.component.css']
 })
 export class CustomerUploadComponent implements OnInit {
-
-  constructor() { }
-
+  file?:File
+  constructor(private http:HttpClient) { }
   ngOnInit(): void {
   }
-
+  upload()
+  {
+    console.log("hi");
+    this.http.get('http://localhost:3000/upload?file='+this.file).subscribe((response) => {
+      console.log('response received is ', response);
+ });
+  }
 }
